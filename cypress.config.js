cc059796c54697cf9d30f80f5afd04a6f380b445
Oleksandr Ohryzko <sasha.ogrizko@gmail.com>
2023-06-06 13:02:19 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -9,13 +9,37 @@ module.exports = defineConfig({
         generateUser() {
           const email = faker.internet.email();
           const randomNumber = Math.floor(Math.random(1000) * 1000);
+          const username = faker.name.firstName() + randomNumber;
           return {
-            username: faker.name.firstName() + randomNumber,
+            username: username.toLowerCase(),
             email: email.toLowerCase(),
             password: '12345Qwert!'
-          };
-        }
+          };}, 
+        generateArticle() {
+            const title = faker.name.firstName();
+            const randomNumber = Math.floor(Math.random(1000) * 1000);
+            return {
+              title: faker.name.firstName() + randomNumber,
+              desc: faker.name.firstName(),
+              body: faker.name.firstName(),
+            };            
+          }   
+          
       });
+      // on('task', {
+      //   generateArticle() {
+      //     const title = faker.name.fullName();
+      //     const randomNumber = Math.floor(Math.random(1000) * 1000);
+      //     return {
+      //       title: faker.name.fullName() + randomNumber,
+      //       desc: faker.name.Name(),
+      //       body: faker.name.Name(),
+      //     };
+          
+      //   }
+
+        
+      // });
     }
   }
 });
--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,15 +1,50 @@
-describe('', () => {
+/// <reference types= "cypress" /> 
+
+describe('Article', () => {
+  let user;
+  let article;
+
   before(() => {
     cy.task('generateUser').then((generateUser) => {
       user = generateUser;
     });
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
   });
 
-  it('', () => {
-
+  it('should be created using New Article form', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('/editor');
+    cy.get('input[placeholder*="Article Title"]')
+      .type(article.title);
+    cy.get('input[placeholder*="What\'s this article about?"]')
+      .type(article.desc);
+      cy.get('textarea.form-control[placeholder="Write your article (in markdown)"]')
+      .type(article.body);
+    cy.get('.btn')
+      .click();
+    cy.get('h1')
+      .should('contain' , article.title)      
   });
 
-  it('', () => {
-
+  it.only('should be deleted using article page', () => {
+    cy.visit('https://conduit.mate.academy/');
+    cy.login(user.email, user.username, user.password);
+    cy.createArticle(article.title, article.desc, article.body);
+    cy.visit(`https://conduit.mate.academy/profile/${user.username}`);
+    cy.get('h1')
+      .should('contain', article.title)
+      .click();
+    cy.contains('button.btn.btn-outline-danger.btn-sm', 'Delete Article')
+      .click();
+    cy.on("window:confirm", (s) => {
+      return true;
+    });
+    cy.visit(`https://conduit.mate.academy/profile/${user.username}`);
+    cy.reload();
+    cy.get('h1')
+      .should('not.exist')
+    });
   });
-});
+
