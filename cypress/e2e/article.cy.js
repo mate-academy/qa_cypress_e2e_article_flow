d24ece57253f367ec076a734b42d222ba05f5f55
yuliiapyrih <yuliia.pyrih.pm.2018@lpnu.ua>
2023-06-06 14:27:14 +0300

    Resolve tasks

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,15 +1,35 @@
+/// <reference types= "cypress" />
+
 describe('', () => {
+  let user;
+  let article;
+
   before(() => {
     cy.task('generateUser').then((generateUser) => {
       user = generateUser;
     });
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
   });
 
-  it('', () => {
+  it('should provide an ability to create an article', () => {
+    cy.login(user.email ,user.username, user.password);
+    cy.visit('/editor');
 
+    cy.getPlaceholder('Article Title')
+      .type(article.title);
+    cy.getPlaceholder('What\'s this article about?')
+      .type(article.description);
+    cy.getPlaceholder('Write your article (in markdown)')
+      .type(article.body);
+    cy.contains('.btn', 'Publish Article')
+      .click();
   });
 
-  it('', () => {
-
+  it('should provide an ability to delete an article', () => {
+    cy.login(user.email ,user.username, user.password);
+    cy.visit('/editor');
+    cy.createArticle(article.title, article.description, article.body);
   });
 });
