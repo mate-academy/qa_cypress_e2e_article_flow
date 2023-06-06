fcbcf9835debf5754514aca069e2c7255d1b678f
Danylo Kravtsov <kravtsovdania@gmail.com>
2023-06-06 13:02:54 +0300

    Solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,15 +1,41 @@
-describe('', () => {
-  before(() => {
+/// <reference types='cypress' />
+
+describe('Conduit', () => {
+  let user;
+  let article;
+
+  beforeEach(() => {
+    cy.visit('/');
     cy.task('generateUser').then((generateUser) => {
       user = generateUser;
     });
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
   });
 
-  it('', () => {
-
+  it('should allows logged in user to create an article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.reload();
+    cy.contains('.nav-link', 'New Article').click();
+    cy.getPlaceholder('Article Title').type(article.title);
+    cy.getPlaceholder('What\'s this').type(article.description);
+    cy.getPlaceholder('Write your').type(article.body);
+    cy.contains('button', 'Publish Article').click();
+    cy.get('h1').should('contain', article.title);
+    cy.get('.col-md-12').should('contain', article.body);
   });
 
-  it('', () => {
-
+  it('should allows logged in user to delete his article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.reload();
+    cy.createArticle(article.title, article.description, article.body);
+    cy.reload();
+    cy.contains('a', 'Global Feed').click();
+    cy.contains('h1', article.title).click();
+    cy.contains('button', ' Delete Article')
+      .eq(0)
+      .click();
+    cy.get('.article-preview').should('contain', 'No articles are here... yet.');
   });
 });
