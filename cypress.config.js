189f1c7cfb05bb14d78d1b9e3a3b3f68e62f0d3b
Maria Dusyk <fibrmirr@gmail.com>
2023-06-07 18:21:44 +0300

    Create/Delete article

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,27 @@
 const { defineConfig } = require("cypress");
 const faker = require('faker');
-
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://conduit.mate.academy/',
     setupNodeEvents(on, config) {
-      on('task', {
-        generateUser() {
-          const email = faker.internet.email();
-          const randomNumber = Math.floor(Math.random(1000) * 1000);
-          return {
-            username: faker.name.firstName() + randomNumber,
-            email: email.toLowerCase(),
-            password: '12345Qwert!'
-          };
-        }
-      });
-    }
+       on('task', {
+         generateUser() {
+           const email = faker.internet.email();
+           const randomNumber = Math.floor(Math.random(1000) * 1000);
+           return {
+             username: faker.name.firstName() + randomNumber,
+             email: email.toLowerCase(),
+             password: '12345Qwert!'
+           };
+         },
+         generateArticle() {
+           return {
+             title: faker.random.word(),
+             description: faker.random.words(),
+             body: faker.random.words()
+           };
+         }
+       });
+     }
   }
 });
