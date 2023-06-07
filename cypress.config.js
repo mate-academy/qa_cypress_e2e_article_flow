d8c339d5a57118d394dde85ea56c3a04d2a394fa
Iryna <novydom23@gmail.com>
2023-06-07 16:08:42 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://conduit.mate.academy/',
+    baseUrl: 'https://conduit.mate.academy',
     setupNodeEvents(on, config) {
       on('task', {
         generateUser() {
@@ -14,7 +14,15 @@ module.exports = defineConfig({
             email: email.toLowerCase(),
             password: '12345Qwert!'
           };
-        }
+        },
+        generateArticle() {
+          return {
+            title: faker.lorem.words(),
+            description: faker.lorem.words(),
+            body: faker.lorem.words(),
+            tag: faker.lorem.word()
+          };
+        },
       });
     }
   }
