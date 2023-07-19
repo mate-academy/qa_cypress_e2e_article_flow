c2dece3db18dd1e4112c086ee2fd4d0693b5e9e2
Tolik Serhiychuk <sergiytchoukk@gmail.com>
2023-07-19 16:19:46 +0300

    solution for article flow

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,57 @@
-describe('', () => {
-  before(() => {
+/* eslint-disable cypress/no-force */
 
+describe('Article flow', () => {
+  let article;
+
+  beforeEach(() => {
+    cy.task('newArticle').then((newArticle) => {
+      article = newArticle;
+    });
+
+    cy.loginConduit();
   });
 
-  it('', () => {
+  it('should allow to create an article', () => {
+    cy.get('[href="/editor"]')
+      .click();
 
+    cy.findByPlaceholder('Article Title')
+      .type(article.title);
+    cy.findByPlaceholder('What\'s this article about?')
+      .type(article.description);
+    cy.findByPlaceholder('Write your article (in markdown)')
+      .type(article.body);
+    cy.findByPlaceholder('Enter tags')
+      .type(article.tag);
+    cy.contains('button', 'Publish Article')
+      .click({ force: true });
+
+    cy.contains('h1', article.title)
+      .should('be.visible');
+    cy.url()
+      .should('include', 'article');
   });
 
-  it('', () => {
+  it('should allow to delete an article', () => {
+    cy.get('[href="/editor"]')
+      .click();
 
+    cy.findByPlaceholder('Article Title')
+      .type(article.title);
+    cy.findByPlaceholder('What\'s this article about?')
+      .type(article.description);
+    cy.findByPlaceholder('Write your article (in markdown)')
+      .type(article.body);
+    cy.findByPlaceholder('Enter tags')
+      .type(article.tag);
+    cy.contains('button', 'Publish Article')
+      .click({ force: true });
+    cy.contains('button', 'Delete Article')
+      .click();
+
+    cy.contains('a', 'Your Feed')
+      .should('be.visible');
+    cy.get('.article-preview')
+      .should('contain.text', 'No articles are here... yet.');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,16 +24,17 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('login', (email, username, password) => {
-  cy.request('POST', '/api/users', {
+// eslint-disable-next-line
+Cypress.Commands.add('loginConduit', (email = 'testing@gmail.com', password = '12345') => {
+  cy.request('POST', 'https://conduit.mate.academy/api/users/login', {
     user: {
       email,
-      username,
       password
     }
-  }).then(response => {
+  }).then((response) => {
     const user = {
       bio: response.body.user.bio,
+      // eslint-disable-next-line
       effectiveImage: 'https://static.productionready.io/images/smiley-cyrus.jpg',
       email: response.body.user.email,
       image: response.body.user.image,
@@ -43,6 +44,11 @@ Cypress.Commands.add('login', (email, username, password) => {
     window.localStorage.setItem('user', JSON.stringify(user));
     cy.setCookie('auth', response.body.user.token);
   });
+  cy.visit('/');
+});
+
+Cypress.Commands.add('findByPlaceholder', (placeholder) => {
+  cy.get(`[placeholder="${placeholder}"]`);
 });
 
 Cypress.Commands.add('createArticle', (title, description, body) => {
