8e5ad53398e2ac0e219263bac6efe70da3b906f5
Olha Mykhats <120174942+olga-mykhats@users.noreply.github.com>
2023-07-19 22:39:47 +0300

    Delete index.d.ts.ts

--- index.d.ts.ts
+++
@@ -1,6 +0,0 @@
-declare namespace Cypress {
-    interface Chainable<Subject> {
-      login(username: string, password: string): Chainable<any>
-      findByPlaceholder(placeholder: string): Chainable<any>
-    }
-  }
