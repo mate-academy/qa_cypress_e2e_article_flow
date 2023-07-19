00dbb385c3697761e6b320fcf9ced0ecb2f340ec
OleksandraLevytska01 <alexandralevytska@gmail.com>
2023-07-19 20:31:51 +0300

    Solution task5

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,48 @@
-describe('', () => {
-  before(() => {
+/// <reference types = "cypress" />
 
+/// <reference types = "../support"/>
+
+describe('Article flow', () => {
+  let article;
+  let user;
+  beforeEach(() => {
+    cy.visit('https://conduit.mate.academy/');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
   });
 
-  it('', () => {
-
+  it('should allow to crate article ', () => {
+    cy.loginConduit(user.email, user.username, user.password);
+    cy.visit('https://conduit.mate.academy/editor');
+    cy.findByPlaceholder('Article Title').type(article.title);
+    cy.findByPlaceholder('What\'s this article about?')
+      .type(article.description);
+    cy.findByPlaceholder('Write your article (in markdown)').type(article.body);
+    cy.findByPlaceholder('Enter tags').type(article.tags);
+    // eslint-disable-next-line cypress/no-force
+    cy.contains('button', 'Publish Article').click({ force: true });
+    cy.get('h1').should('contain', article.title);
   });
 
-  it('', () => {
-
+  it('should allow to delete the article', () => {
+    cy.loginConduit(user.email, user.username, user.password);
+    cy.visit('https://conduit.mate.academy/editor');
+    cy.findByPlaceholder('Article Title').type(article.title);
+    cy.findByPlaceholder('What\'s this article about?')
+      .type(article.description);
+    cy.findByPlaceholder('Write your article (in markdown)')
+      .type(article.body);
+    cy.findByPlaceholder('Enter tags').type(article.tags);
+    // eslint-disable-next-line cypress/no-force
+    cy.contains('button', 'Publish Article').click({ force: true });
+    cy.get('h1').should('contain', article.title);
+    cy.contains('button', 'Delete Article').click();
+    cy.contains('a', 'Your Feed').should('be.visible');
+    cy.get('.article-preview')
+      .should('contain.text', 'No articles are here... yet.');
   });
 });
