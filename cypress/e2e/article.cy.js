4d65968b4cfbb472bacedf702422247cfb311b14
Yurii Rudyi <georgerichie2018@gmail.com>
2023-09-21 18:13:38 +0300

    Solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,47 @@
-describe('', () => {
-  before(() => {
+describe('Article flow', () => {
+  let user;
+  let article;
+
+  beforeEach(() => {
+    cy.visit('/');
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
 
   });
 
-  it('', () => {
-
+  it('should create an article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('/editor');
+    cy.findByPlaceholder('Article Title').type(article.title);
+    cy.findByPlaceholder('What\'s this article about?').type(article.description);
+    cy.findByPlaceholder('Write your article (in markdown)').type(article.body);
+    cy.findByPlaceholder('Enter tags').type(article.tags);
+    cy.contains('button', 'Publish Article').click({ force: true });
+    cy.contains('h1', article.title).should('be.visible');
+    cy.url().should('include', 'article');
   });
 
-  it('', () => {
-
+  it('should delete an article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('/editor');
+    cy.findByPlaceholder('Article Title').type(article.title);
+    cy.findByPlaceholder('What\'s this article about?').type(article.description);
+    cy.findByPlaceholder('Write your article (in markdown)').type(article.body);
+    cy.findByPlaceholder('Enter tags').type(article.tags);
+    cy.contains('button', 'Publish Article').click({ force: true });
+    cy.contains('h1', article.title).should('be.visible');
+    cy.url().should('include', 'article');
+    cy.contains('button', 'Delete Article').click();
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.equal('Do you really want to delete it?');
+      return true;
+    });
+    cy.get('.article-preview').should('contain', 'No articles are here... yet.');
+    cy.url().should('include', '/');
   });
 });
