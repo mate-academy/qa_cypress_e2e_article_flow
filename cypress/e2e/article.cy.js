21ac80260aede47e60ac444b511ee9c15c5fb9e4
VetKochetkov <bet4mail@gmail.com>
2023-09-21 15:57:35 +0200

    Cypress-Article-flow

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,63 @@
-describe('', () => {
-  before(() => {
+let article;
+let user;
 
+describe('Create and Delete an article', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.task('generateUser')
+      .then((generateUser) => {
+        user = generateUser;
+      });
+    cy.task('articleNew')
+      .then((articleNew) => {
+        article = articleNew;
+      });
   });
 
-  it('', () => {
-
+  it('should allow to create an article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('/editor');
+    cy.findByPlaceholder('Article Title')
+      .type(article.title);
+    cy.findByPlaceholder('What\'s this article about?')
+      .type(article.description);
+    cy.findByPlaceholder('Write your article (in markdown)')
+      .type(article.body);
+    cy.findByPlaceholder('Enter tags')
+      .type(article.tag);
+    cy.contains('button', 'Publish Article')
+      .click({ force: true });
+    cy.contains('h1', article.title)
+      .should('be.visible');
+    cy.url()
+      .should('include', 'article');
   });
 
-  it('', () => {
-
+  it('should allow to delete the article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('/editor');
+    cy.findByPlaceholder('Article Title')
+      .type(article.title);
+    cy.findByPlaceholder('What\'s this article about?')
+      .type(article.description);
+    cy.findByPlaceholder('Write your article (in markdown)')
+      .type(article.body);
+    cy.findByPlaceholder('Enter tags')
+      .type(article.tag);
+    cy.contains('button', 'Publish Article')
+      .click({ force: true });
+    cy.contains('h1', article.title)
+      .should('be.visible');
+    cy.url()
+      .should('include', 'article');
+    cy.contains('button', 'Delete Article')
+      .click();
+    cy.on('window:alert', (alert) => {
+      expect(alert).to.equal('Do you really want to delete it?');
+      return true;
+    });
+    cy.get('.article-preview')
+      .should('contain', 'No articles are here... yet.');
+    cy.url().should('include', '/');
   });
 });
