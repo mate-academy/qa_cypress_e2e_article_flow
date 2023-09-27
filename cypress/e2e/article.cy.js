0361925d3cf4605c63a8abf42b7fbdcf123eaf76
Julia14012000 <Juliamilan1414@gmail.com>
2023-09-27 09:39:35 +0300

    e2e Article flow

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,45 @@
-describe('', () => {
-  before(() => {
+describe('Article creation and deletion', () => {
+  let user;
+  let article;
 
+  beforeEach(() => {
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
   });
 
-  it('', () => {
+  it('should provide an ability to create a new article', () => {
+    cy.registrationAndLogin(user.email, user.username, user.password);
+    cy.visit('/editor');
 
+    cy.findbyPlaceholder('Article Title').type(article.title);
+    cy.findbyPlaceholder('What\'s this article about?')
+      .type(article.description);
+    cy.findbyPlaceholder('Write your article (in markdown)')
+      .type(article.body);
+    cy.contains('Publish Article').click();
   });
 
-  it('', () => {
-
+  it('should provide an ability to delete an article', () => {
+    cy.createArticle(
+      user.username,
+      user.email,
+      user.password,
+      article.title,
+      article.description,
+      article.body
+    ).then((response) => {
+      cy.visit(`/article/${response.body.article.slug}`);
+    });
+    cy.get('.article-actions').contains('Delete Article').click();
+    cy.on('window:confirm', (alert) => {
+      expect(alert).to.equal('Do you really want to delete it?');
+      return true;
+    });
+    cy.get('.article-preview')
+      .should('contain', 'No articles are here... yet.');
   });
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,19 @@ import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.Commands.add('login', (username, password) => {
+  cy.request({
+    method: 'POST',
+    url: 'https://demoqa.com/Account/v1/Login',
+    body: {
+      userName: username,
+      password
+    }
+  }).then((response) => {
+    cy.setCookie('token', response.body.token);
+    cy.setCookie('userID', response.body.userId);
+    cy.setCookie('expires', response.body.expires);
+    cy.setCookie('userName', response.body.username);
+  });
+});
