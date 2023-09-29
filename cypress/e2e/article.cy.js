c1c878e050b2de02ec001a883d036d4b6304238f
Dmytro Ponomarov <ponomarovdmitry13@gmail.com>
2023-09-29 12:05:06 +0300

    Solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -18,14 +18,19 @@ describe('', () => {
     cy.findByPlaceholder('What\'s this article about?').type(article.body);
     cy.findByPlaceholder('Write your article (in markdown)').type(article.description);
     cy.get('.btn').contains('Publish').click();
+    cy.get('h1').should('contain', article.title);
+    cy.get('.row.article-content').should('contain', article.description);
   });
 
-  it('should be Delete Article', () => {
+  it.only('should be Delete Article', () => {
     cy.login(user.username, user.email, user.password);
     cy.createArticle(article.title, article.body, article.description)
       .then((response => {
         cy.visit(`/article/${response.body.article.slug}`);
       }));
     cy.get('.btn').contains(' Delete Article').click();
+    cy.get('.link').contains('Global Feed').click();
+    cy.get('.row').should('not.contain', article.title);
+    cy.get('.row').should('not.contain', article.description);
   });
 });
