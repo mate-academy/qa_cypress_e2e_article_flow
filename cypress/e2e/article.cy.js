c419d959d762c6fd2a22669087ff7b23e9d1e8e6
okan3 <okan_ivan@ukr.net>
2023-11-09 19:01:44 +0200

    add automation test for app

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,46 @@
-describe('', () => {
-  before(() => {
+describe('Conduit app', () => {
+  let user;
+  let article;
 
+  beforeEach(() => {
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
   });
 
-  it('', () => {
-
+  it('login', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('/editor');
+    cy.get('[placeholder="Article Title"]')
+      .type(article.title)
+      .should('have.value', article.title);
+    cy.get('[placeholder="What\'s this article about?"]')
+      .type(article.description)
+      .should('have.value', article.description);
+    cy.get('[placeholder="Write your article (in markdown)"]')
+      .type(article.body)
+      .should('have.value', article.body);
+    cy.get('[placeholder="Enter tags"]')
+      .type(article.tag)
+      .should('have.value', article.tag)
+    cy.get('.btn')
+      .click()
+      .should('be.visible');
+    cy.get('.btn')
+      .should('contain', 'Publish Article');
   });
 
-  it('', () => {
-
+  it('Delete', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.createArticle(article.title, article.description, article.body)
+      .then((response) => {
+        cy.visit(`article/${response.body.article.slug}`);
+        cy.get('.btn').contains('Delete Article').click();
+        cy.get('.article-preview')
+          .should('contain', 'No articles are here... yet.');
+      });
   });
 });
