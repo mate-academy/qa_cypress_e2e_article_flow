43f9a839eba1e09b184ccc06215e6fef2df0ed75
KlymSudakov <sudakov.klym@gmail.com>
2023-11-09 17:56:15 +0200

    Implement article tests

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,52 @@
-describe('', () => {
-  before(() => {
+/// <reference types='cypress' />
 
+describe('Article', () => {
+  let user = '';
+  let article = '';
+
+  beforeEach(() => {
+    cy.visit('/');
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+      cy.login(user.email, user.username, user.password);
+    });
   });
 
-  it('', () => {
+  it('should provide an ability to create an article', () => {
+    cy.createArticle(article.title, article.description, article.body)
+      .then((response) => {
+        cy.visit(`/article/${response.body.article.slug}`);
+      });
 
+    cy.get('.banner')
+      .should('contain', article.title);
+    cy.get('.article-content')
+      .should('contain', article.body);
+    cy.get('article-meta')
+      .should('contain', 'Edit Article');
+    cy.get('article-meta')
+      .should('contain', 'Delete Article');
   });
 
-  it('', () => {
+  it('should provide an ability to delete an article', () => {
+    cy.createArticle(article.title, article.description, article.body)
+      .then((response) => {
+        cy.visit(`/article/${response.body.article.slug}`);
+        cy.contains('.btn', 'Delete Article')
+          .click();
+        cy.on('window:confirm', (text) => {
+          expect(text).to.equal('Do you really want to delete it?');
 
+          return true;
+        });
+
+        cy.get('.article-preview')
+          .should('contain', 'No articles are here... yet.');
+        cy.url()
+          .should('equal', 'https://conduit.mate.academy/');
+      });
   });
 });
