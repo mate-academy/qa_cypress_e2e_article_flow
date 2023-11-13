65d9db4480ec275c6373a6aa31df7188f6f2ebb7
Alex <horonhozov@gmail.com>
2023-11-13 16:17:20 +0200

    demo

--- cypress/e2e/demoblaze.cy.js
+++ cypress/e2e/demoblaze.cy.js
@@ -1,34 +1,40 @@
-// cypress/integration/demoBlazeTests.spec.js
+/// <reference types='cypress' />
 
-describe('DemoBlaze E2E Tests', () => {
-   it('should register a new user', () => {
-      cy.visit('https://www.demoblaze.com/');
-      cy.get('#signin2').click();
-      cy.get('#sign-username').type('operada1');
-      cy.get('#sign-password').type('opera123$');
-      cy.contains('.btn', 'Sign up').click();
-      cy.on('window:alert', (str) => {
-        expect(str).to.equal('Sign up successful.');
-      });
-    });
+describe('User\'s flow', () => {
+   beforeEach(() => {
+       cy.visit('https://www.demoblaze.com/')
+   });
 
-   it('should login', () => {
-     cy.visit('https://www.demoblaze.com/');
-     cy.get('#login2').click();
-     cy.get('#loginusername').type('operada1');
-     cy.get('#loginpassword').type('opera123$');
-     cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
-     cy.get('.nav-link')
-      .should('contain', 'Welcome operada1');
+   it('should provide an ability to register the user', () => {
+       cy.get('#signin2')
+           .click();
+       cy.get('#sign-username')
+           .type('qasep23');
+       cy.get('#sign-password')
+           .type('Qwer123!');
+       cy.get('.btn-primary').contains('Sign up')
+           .click();
    });
   
-   it('should add Samsung Galaxy s6 to the cart', () => {
-     cy.visit('https://www.demoblaze.com/');
-     cy.get(':nth-child(7) > .card > .card-block > .card-title > .hrefch').click();
-     cy.get('.col-sm-12 > .btn').click();
-     cy.on('window:alert', (str) => {
-       expect(str).to.equal('Product added.');
-     });
+   it('should provide an ability to log in with valid credentials', () => {
+       cy.get('#login2')
+           .click();
+       cy.get('#loginusername')
+           .type('qasep23');
+       cy.get('#loginpassword')
+           .type('Qwer123!');
+       cy.get('.btn-primary').contains('Log in')
+           .click();
    });
- });
- 
+
+   it('should provide an ability to add Samsung Galaxy s6 to the cart', () => {
+       cy.get(':nth-child(1) > .card > .card-block > .card-title > .hrefch')
+           .contains('Samsung galaxy s6')
+           .click();
+       cy.get('.col-sm-12 > .btn').contains('Add to cart')
+           .click();
+       cy.on('window:alert', (str) => {
+       expect(str).to.equal('Product added');
+       });
+   });
+});
