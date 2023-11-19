a05d74db3881ef1f1a2037e5e2350e206762cde3
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-19 08:23:14 +0200

    article.cy.js

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,25 @@
-describe('', () => {
+describe('Article flow', () => {
+  let user;
+  let article;
+
   before(() => {
-
+    cy.fixture('example.json').then((data) => {
+      user = data;
+      cy.login(user.email, user.password);
+    });
   });
 
-  it('', () => {
-
+  it('Create the article', () => {
+    cy.createArticle(user.name, user.body).then((response) => {
+      article = response.body.article;
+      cy.visit(`/article/${article.slug}`);
+      cy.get('h1').should('contain', user.name);
+    });
   });
 
-  it('', () => {
-
+  it('Delete the article', () => {
+    cy.deleteArticle(article.slug);
+    cy.visit(`/article/${article.slug}`);
+    cy.get('h1').should('not.exist');
   });
 });
