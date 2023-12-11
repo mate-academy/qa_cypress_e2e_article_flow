a34fdf56ad99736bc83d1ca51be53e755c4b1fbe
Noemi <noemi.anna.fic@gmail.com>
2023-12-11 20:49:52 +0100

    article flow

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,17 @@
 describe('', () => {
+  let user;
   before(() => {
-
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
+    });
   });
 
-  it('', () => {
-
-  });
-
-  it('', () => {
-
+  it('should provide an ability to create the article', () => {
+    cy.visit('/user/register');
+    cy.login(user);
+    cy.getCookie('auth').should('exist');
+    cy.createArticle().then((articleInfo) => {
+      cy.deleteArticle(articleInfo);
+    });
   });
 });
