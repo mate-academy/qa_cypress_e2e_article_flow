c3d018b4552b793583e25004b70c6d22987fce79
Tomasz Kubicz <kubicz.t@yahoo.com>
2023-12-11 14:04:15 +0100

    Solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,19 @@
-describe('', () => {
+describe('article flow test', () => {
   before(() => {
-
+    cy.task('generateUser').then((user) => {
+      cy.login(user.email, user.username, user.password);
+    });
   });
 
-  it('', () => {
+  it('should create and delete an article', () => {
+    cy.visit('https://conduit.mate.academy/editor');
 
-  });
-
-  it('', () => {
-
+    it('should create and delete an article', () => {
+      // Create an article and capture the article slug
+      cy.createArticle('Test Article',
+        'This is a test description', 'Test article content');
+      // Delete the created article
+      cy.deleteArticle();
+    });
   });
 });
