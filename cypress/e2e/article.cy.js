bb27af3a59087e6100d34bf20c74616c4c62edee
Paweł Grzywna <pawelgrzywna01@outlook.com>
2023-12-14 00:43:17 +0100

    Solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,69 @@
-describe('', () => {
-  before(() => {
+/// <reference types="cypress" />
 
+const { faker } = require('@faker-js/faker');
+
+describe('article', () => {
+  beforeEach(() => {
+    cy.visit('user/register');
   });
 
-  it('', () => {
-
+  it('should create an article', () => {
+    const randomNumber = Math.random().toString().slice(2, 10);
+    const user = {
+      username: faker.lorem.word() + randomNumber,
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+    const article = {
+      title: faker.lorem.word(),
+      description: faker.lorem.words(3),
+      body: faker.lorem.words(10)
+    };
+    cy.login(user.email, user.username, user.password);
+    cy.createArticle(article.title, article.description, article.body)
+      .as('article');
+    cy.get('@article').should((resp) => {
+      expect(resp.status).to.eq(200);
+    });
   });
 
-  it('', () => {
+  it('should delete the created article', () => {
+    const randomNumber = Math.random().toString().slice(2, 10);
+    const user = {
+      username: faker.lorem.word() + randomNumber,
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+    const article = {
+      title: faker.lorem.word(),
+      description: faker.lorem.words(3),
+      body: faker.lorem.words(10)
+    };
+    cy.login(user.email, user.username, user.password);
+    cy.createArticle(article.title, article.description, article.body)
+      .then((resp) => {
+        cy.getCookie('auth').then((token) => {
+          const authToken = token.value;
 
+          cy.request({
+            method: 'DELETE',
+            url: `/api/articles/${resp.body.article.slug}`,
+            body: {
+              article: {
+                title: resp.body.article.title,
+                description: resp.body.article.description,
+                body: resp.body.article.body,
+                tagList: []
+              }
+            },
+            headers: {
+              Authorization: `Token ${authToken}`
+            }
+          });
+        });
+      }).as('delete');
+    cy.get('@delete').should((resp) => {
+      expect(resp.status).to.eq(204);
+    });
   });
 });
