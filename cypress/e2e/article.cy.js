5fec80272014138a1608cd720efe4855ba687f49
MagdaK <makolasze@gmail.com>
2023-12-25 19:47:28 +0100

    Article flow

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,23 @@
-describe('', () => {
+describe('Conduit article', () => {
+  let user;
+  let article;
+
   before(() => {
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
+    });
 
+    cy.task('generateArticle').then((generatedArticle) => {
+      article = generatedArticle;
+    });
   });
 
-  it('', () => {
-
-  });
-
-  it('', () => {
-
+  it('should create an article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.createArticle(article.title, article.description, article.body);
+    cy.visit('profile/' + (user.username).toLowerCase());
+    cy.get('h1').click();
+    cy.get('button').contains('Delete Article').click();
+    cy.get('.article-preview').should('contain', 'No articles are here... yet.');
   });
 });
