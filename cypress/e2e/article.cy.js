c87fa40f92e4abcbb363ab9d650965c3b4a0239c
Tomasz Kubicz <kubicz.t@yahoo.com>
2023-12-28 10:42:26 +0100

    Fixed solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -15,7 +15,13 @@ describe('Conduit article', () => {
     cy.login(user.email, user.username, user.password);
     cy.reload();
     cy.get('.navbar').should('contain', user.username.toLowerCase());
-    cy.createArticle(article.title, article.description, article.body);
+    // cy.createArticle(article.title, article.description, article.body);
+    cy.get('a.nav-link[href="/editor"]').click();
+    cy.get('input[placeholder="Article Title"]').type(article.title);
+    cy.get('input[placeholder="What\'s this article about?"]')
+      .type(article.description);
+    cy.get('fieldset.form-group textarea.form-control').type(article.body);
+    cy.get('button.btn.btn-lg.pull-xs-right.btn-primary').click();
     cy.contains('a', user.username.toLowerCase()).click();
     cy.get('.row').should('contain', article.title);
     cy.get('.row').should('contain', article.description);
