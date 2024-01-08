6f0f289f6892a1455d544205257814132fb31461
Rogers1275 <sashakondyuk@gmail.com>
2024-01-08 16:41:33 +0200

    added test for creatinf article via ui

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -16,11 +16,13 @@ describe('Conduit', () => {
 
   it('should provide an ability to create an article', () => {
     cy.login(user.email, user.username, user.password);
-    cy.createArticle(article.title, article.description, article.body);
-    cy.visit('/');
-    cy.get('[alt="your profile image"]').click();
+    cy.visit('/editor');
+    cy.get('[placeholder="Article Title"]').type(article.title);
+    cy.get('[placeholder="What\'s this article about?"]').type(article.description);
+    cy.get('[placeholder="Write your article (in markdown)"]').type(article.body);
+    cy.get('.btn.btn-lg.pull-xs-right.btn-primary').click();
     
-    cy.get('.article-preview').should('contain', article.title);
+    cy.get('.banner').should('contain', article.title);
   });
 
   it('should provide an ability to delete an article', () => {
