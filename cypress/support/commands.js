78f865bdb5c52e291a3f7eb362f8362edf48e09f
Yura Kisilchuk <yurkis02@gmail.com>
2024-01-08 14:48:47 +0200

    added e2e tests for the Article flow

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,7 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
 Cypress.Commands.add('login', (email, username, password) => {
   cy.request('POST', '/api/users', {
     user: {
