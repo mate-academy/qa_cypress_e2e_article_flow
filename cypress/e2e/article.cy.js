7ae128e4ce9dc3114542de7d831d9b995d5893c3
Yuliia Siromakha <yuliya.o.siromakha@gmail.com>
2024-01-08 18:01:25 +0200

    tests

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,59 @@
-describe('', () => {
-  before(() => {
+
+let article
+describe('Actions with articles', () => {
+  beforeEach(() => {
+    cy.login();
+    cy.task('createArticle').then(createArticle => {
+      article = createArticle;
+   });
+  });
+
+  it('should provide an ability to create an article', () => {
+    cy.visit('/');
+    cy.get('[href="/editor"]').click();
+    //cy.createArticle();
+    cy.findByPlaceholder('Article Title').type(article.title);
+    cy.findByPlaceholder('What\'s this article about?').type(article.description);
+    cy.findByPlaceholder('Write your article (in markdown)').type(article.articleBody);
+    cy.findByPlaceholder('Enter tags').type(article.tag, {timeout: 1000});
+    cy.contains('[type="button"]', 'Publish Articl').click({force: true});
+    cy.get('.container').should('contain', article.title);
+
+
+    
 
   });
 
-  it('', () => {
+  it.only('should provide an ability to delete an article', () => {
+   
 
-  });
+    cy.createArticle()
+    .then((responce) => {
+      const slug = responce.body.article.slug;
+      cy.visit(`article/${slug}`);
 
-  it('', () => {
+    });
+
+
+    //cy.get('[href="/editor"]').click();
+    
+      //cy.findByPlaceholder('Article Title').type(article.title);
+      //cy.findByPlaceholder('What\'s this article about?').type(article.description);
+      //cy.findByPlaceholder('Write your article (in markdown)').type(article.articleBody);
+      //cy.findByPlaceholder('Enter tags').type(article.tag, {timeout: 1000});
+      //cy.contains('[type="button"]', 'Publish Articl').click({force: true});
+    
+    cy.contains('[class="btn btn-outline-danger btn-sm"]', 'Delete Article').click();
+    cy.on('window:confirm', (text) => {
+      expect(text).to.contains('Do you really want to delete it?');
+      return true;
+    });
+
+    cy.get('[class="hide-text user-pic"]').click({force: true});
+    cy.get('[class="hide-text user-pic"]').click({force: true});
+    cy.get('[class="preview-link"]').should('not.contain', article.title);
+
+
 
   });
 });
