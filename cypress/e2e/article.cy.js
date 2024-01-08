8ccc91267ea4d9676520729a5008043b6fc34f7e
Orest Halushka <halslor@gmail.com>
2024-01-08 14:24:38 +0200

    Solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,27 @@
-describe('', () => {
-  before(() => {
+describe('Conduit app', () => {
+  let user;
+  let article;
 
+  beforeEach(() => {
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+      cy.task('generateArticle').then((generateArticle) => {
+        article = generateArticle;
+        cy.login(user.email, user.username, user.password);
+        cy.createArticle(article.title, article.description, article.body);
+      });
+    });
   });
 
-  it('', () => {
-
+  it('should be able to create an article', () => {
+    cy.contains('.container', article.title).should('exist');
   });
 
-  it('', () => {
-
+  it('should be able to delete an article', () => {
+    cy.contains('.btn', 'Delete Article').click();
+    cy.on('window:confirm', (text) => {
+      expect(text).to.equal('Do you really want to delete it?');
+    });
+    cy.get('.article-preview').should('contain', 'No articles are here');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,28 +1,4 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import faker from 'faker';
 
 Cypress.Commands.add('login', (email, username, password) => {
   cy.request('POST', '/api/users', {
@@ -31,10 +7,11 @@ Cypress.Commands.add('login', (email, username, password) => {
       username,
       password
     }
-  }).then(response => {
+  }).then((response) => {
     const user = {
       bio: response.body.user.bio,
-      effectiveImage: 'https://static.productionready.io/images/smiley-cyrus.jpg',
+      effectiveImage: 
+        'https://static.productionready.io/images/smiley-cyrus.jpg',
       email: response.body.user.email,
       image: response.body.user.image,
       token: response.body.user.token,
@@ -46,23 +23,10 @@ Cypress.Commands.add('login', (email, username, password) => {
 });
 
 Cypress.Commands.add('createArticle', (title, description, body) => {
-  cy.getCookie('auth').then((token) => {
-    const authToken = token.value;
-
-    cy.request({
-      method: 'POST',
-      url: '/api/articles',
-      body: {
-        article: {
-          title,
-          description,
-          body,
-          tagList: []
-        }
-      },
-      headers: {
-        Authorization: `Token ${authToken}`
-      }
-    });
-  });
+  cy.visit('/editor');
+  cy.get('[placeholder="Article Title"]').type(title);
+  cy.get('[placeholder="What\'s this article about?"]').type(description);
+  cy.get('[placeholder="Write your article (in markdown)"]').type(body);
+  cy.get('[type="button"]').contains('Publish').click();
+  cy.get('.article-page').should('exist');
 });
