8adc665b26c1df2f5f5be5178aaf373075c65302
MaksymKonosh <konoshm058@gmail.com>
2024-01-09 22:43:43 +0200

    Task is Done episode 2: Attack of the Clones

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,41 +1,40 @@
 describe('Article page', () => {
   let user;
   let article;
+
   beforeEach(() => {
     cy.task('generateUser').then((newUser) => {
       user = newUser;
     });
+
     cy.task('generateArticle').then((newArticle) => {
       article = newArticle;
     });
+
     cy.visit('/');
   });
 
   it('should provide an ability to create a new Article', () => {
     cy.login(user.email, user.username, user.password);
-    cy.visit('editor');
-    cy.get('.nav-link').should('contain', user.username.toLowerCase());
-    cy.findByPlaceholder('Article Title').type(article.title);
-    cy.findByPlaceholder('article about?').type(article.description);
-    cy.findByPlaceholder('Write your article (in markdown)')
+    cy.visit('/editor');
+    cy.get('[placeholder="Article Title"]').type(article.title);
+    cy.get('[placeholder="What\'s this article about?"]')
       .type(article.description);
-    cy.get('[type="button"]').contains('Publish')
-      .click();
-    cy.contains('.container', article.title)
-      .should('exist');
+    cy.get('[placeholder="Write your article (in markdown)"]')
+      .type(article.body);
+    cy.get('.btn.btn-lg.pull-xs-right.btn-primary').click();
+    cy.get('.banner').should('contain', article.title);
   });
 
-  it.only('should provide an ability to delete an article', () => {
+  it('should provide an ability to delete an article', () => {
     cy.login(user.email, user.username, user.password);
     cy.createArticle(article.title, article.description, article.body)
       .then((response) => {
         const slug = response.body.article.slug;
         cy.visit(`article/${slug}`);
       });
-    cy.contains('.btn', 'Delete Article')
-      .click();
 
-    cy.get('.article-preview')
-      .should('contain', 'No articles are here');
+    cy.contains('.btn', 'Delete Article').click();
+    cy.get('.article-preview').should('contain', 'No articles are here');
   });
 });
