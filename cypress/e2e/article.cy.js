b70872c69ef7d6354c9bf0e14b7ea5105f3f657a
Anastasiia <anastassyayassnska13@gmail.com>
2024-02-06 13:04:00 +0200

    Solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,40 @@
-describe('', () => {
-  before(() => {
+describe('Create and delete article', () => {
+  let user;
+  let article;
 
+  beforeEach(() => {
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
   });
 
-  it('', () => {
-
+  it('should create an article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('/editor');
+    cy.findByyPlaceholder('Article Title').type(article.title);
+    cy.findByyPlaceholder(`What's this article about?`)
+      .type(article.description);
+    cy.findByyPlaceholder('Write your article (in markdown)')
+      .type(article.body);
+    cy.findByyPlaceholder('Enter tags')
+      .type(article.tags);
+    cy.contains('[type="button"]', 'Publish Article').click({ force: true });
+    cy.get('[class="banner"]').should('contain', article.title);
   });
 
-  it('', () => {
-
+  it('should delete an article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.createArticle(article.title, article.description, article.body)
+      .then((response) => {
+        const slug = response.body.article.slug;
+        cy.visit(`/article/${slug}`);
+      });
+    cy.contains(' Delete Article')
+      .click();
+    cy.get('.article-preview')
+      .should('contain', 'No articles are here... yet.');
   });
 });
