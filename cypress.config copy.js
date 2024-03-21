ba5cc42284f7410e9c0be6a636bc0942e21acc7e
falatorry <dbgdnv@gmail.com>
2024-03-21 21:57:24 +0200

    Solution by Bohdanov Daniil

--- cypress.config copy.js
+++ cypress.config copy.js
@@ -1,8 +1,25 @@
 const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://conduit.mate.academy',
     setupNodeEvents(on, config) {
-    },
-  },
+      on('task', {
+        generateUser() {
+          return {
+            username: faker.internet.userName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+            article: {
+              title: faker.lorem.word(),
+              about: faker.lorem.words(2),
+              conten: faker.lorem.words(10),
+              tag: faker.lorem.word()
+            }
+          };
+        }
+      });
+    }
+  }
 });
--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,48 @@
+const { faker } = require('@faker-js/faker');
+
 describe('', () => {
-  before(() => {
+  let user;
+  const article = {
+    title: faker.lorem.word(),
+    about: faker.lorem.words(2),
+    content: faker.lorem.words(10),
+    tag: faker.lorem.word()
+  };
 
+  beforeEach(() => {
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
   });
 
-  it('', () => {
+  it('should provide an ability to create an article', () => {
+    cy.login(user.email, user.username, user.password);
 
+    cy.visit('/editor');
+
+    cy.get('[placeholder="Article Title"]').type(article.title);
+
+    cy.get('[placeholder="What\'s this article about?"]').type(article.about);
+
+    cy.get('[placeholder="Write your article (in markdown)"]')
+      .type(article.content);
+
+    cy.get('[placeholder="Enter tags"]').type(`${article.tag}{enter}`);
+
+    cy.get('.btn').click();
+
+    cy.get('h1').should('contain.html', article.title);
+
+    cy.url().should('include', '/article/');
   });
 
-  it('', () => {
+  it('should provide an ability to delete an article', () => {
+    cy.login(user.email, user.username, user.password);
 
+    cy.createArticle(article.title, article.about, article.content);
+
+    cy.get('.ion-trash-a').first().click();
+
+    cy.get('.col-md-9').should('not.contain.text', article.title);
   });
 });
