694b87a6393e2bfcd7aecdc82b2dd361150283ee
Tetiana Maksiuk <tetianamaksiuk@gmail.com>
2024-03-22 12:39:47 +0200

    add fix

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,5 +1,7 @@
 /// <reference types='cypress' />
 
+const { ar } = require("@faker-js/faker");
+
 describe('', () => {
   let user;
 
@@ -13,36 +15,37 @@ describe('', () => {
 
   it('should have the ability to create an article', () => {
     let article;
-    let usernameToLowerCase = user.username.toLowerCase();
     cy.task('generateArticle').then((generateArticle) => {
       article = generateArticle;
-      cy.createArticle(article.title, article.about, article.body);
-      cy.get('.link.nav-link').eq(1).click();
-      cy.contains('.article-preview', article.title).should('exist');
-      cy.contains('.article-preview', article.title)
-        .find('.author')
-        .should('have.text', usernameToLowerCase);
+      cy.get('.ion-compose').click();
+      cy.get('[placeholder="Article Title"]').type(article.title);
+      cy.get('[placeholder="What\'s this article about?"]').type(article.about);
+      cy.get('[placeholder="Write your article (in markdown)"]')
+      .type(article.body);
+      cy.get('button.btn-primary').contains('Publish Article').click();
+      cy.get('button.btn-outline-danger').eq(0).should('exist');
+      cy.get('button.btn-outline-danger').eq(0).should('be.enabled');
+      cy.get('button.btn-outline-danger').eq(1).should('exist');
+      cy.get('button.btn-outline-danger').eq(1).should('be.enabled');
     });
   });
 
   it('should have the ability to delete an article', () => {
     const textConfirmAlert = 'Do you really want to delete it?';
-    const messageForNoPosts = 'No articles are here... yet.';
     let article;
 
     cy.task('generateArticle').then((generateArticle) => {
       article = generateArticle;
       cy.createArticle(article.title, article.about, article.body);
-      cy.get('.nav-item').eq(3).click();
-      cy.get('.preview-link').eq(0).click();
-      cy.get('.ion-trash-a').eq(0).click();
+      cy.get('[alt="your profile image"]').click();
+      cy.contains('.article-preview', article.title).click();
+      cy.get('button.btn-outline-danger').eq(0).click();
       cy.on('window:confirm', (textOfConfirm) => {
         expect(textOfConfirm).to.equal(textConfirmAlert);
         return true;
       });
-
-      cy.get('.nav-item').eq(3).click();
-      cy.get('.article-preview').should('have.text', messageForNoPosts);
+      cy.get('[alt="your profile image"]').click();
+      cy.get('.article-preview').should('not.contain', article.title);
     });
   });
 });
