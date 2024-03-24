0afc7b889674d2502dc585e19d23b495005495e4
Serhii Sharabura <youserhii@gmail.com>
2024-03-24 17:01:01 +0200

    V2

--- cypress.config copy.js
+++ cypress.config copy.js
@@ -3,7 +3,7 @@ const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://conduit.mate.academy',
+    //baseUrl: 'https://conduit.mate.academy',
 
     setupNodeEvents(on, config) {
       on('task', {
--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -13,32 +13,55 @@ describe('This article test', () => {
     body: 'Everybody',
   };
 
-  it('should create a new article', () => {
+  it.skip('should create a new article', () => {
     cy.login(fakeuser.email, fakeuser.username, fakeuser.password);
-    cy.createArticle(article.title, article.description, article.body);
-    cy.reload();
-    cy.contains('a', 'Global Feed').click();
-    cy.contains('h1', article.title).should('contain', article.title);
-    cy.contains('p', article.description).should(
-      'contain',
+    cy.visit('https://conduit.mate.academy');
+    cy.get('[href="/editor"]').click();
+    cy.get('.form-control.form-control-lg').type(article.title);
+    cy.get('[placeholder="What\'s this article about?"]').type(
       article.description
     );
-    cy.contains('.author', fakeuser.username.toLowerCase()).should(
-      'contain',
-      fakeuser.username.toLowerCase()
-    );
+    cy.get('[placeholder^="Write your article"]').type(article.body);
+    cy.get('.btn.btn-lg.pull-xs-right.btn-primary')
+      .click()
+      .then((result) => {
+        cy.wait(2000);
+        cy.contains('.nav-link', 'Home').click();
+        cy.contains('a', 'Global Feed').click();
+        cy.contains('h1', article.title).should('contain', article.title);
+        cy.contains('p', article.description).should(
+          'contain',
+          article.description
+        );
+        cy.contains('.author', fakeuser.username.toLowerCase()).should(
+          'contain',
+          fakeuser.username.toLowerCase()
+        );
+      });
   });
 
   it.only('should delete an article', () => {
     cy.login(fakeuser.email, fakeuser.username, fakeuser.password);
-    cy.reload();
+    cy.visit('https://conduit.mate.academy');
     cy.createArticle(article.title, article.description, article.body);
-    cy.reload();
+    cy.visit('https://conduit.mate.academy');
     cy.contains('a', 'Global Feed').click();
+    cy.intercept(
+      'GET',
+      'https://conduit.mate.academy/_next/data/prXNDmVQlPmg2o-RAU2Mw/article/*'
+    ).as('getArticle');
     cy.contains('h1', article.title).click();
-    cy.contains('button', 'Delete Article').click();
-    cy.contains('.article-preview', 'No articles are here... yet.').should(
-      'be.visible'
-    );
+    cy.wait('@getArticle').then((interception) => {
+      const articleSlug = interception.response.body?.slug;
+      cy.log(`Article Slug: ${articleSlug}`);
+      cy.contains('button', 'Delete Article').click();
+      cy.contains('.article-preview', 'No articles are here... yet.').should(
+        'be.visible'
+      );
+      cy.visit(`https://conduit.mate.academy/article/${articleSlug}`);
+      cy.intercept('/not-found', {
+        statusCode: 404,
+      }).should('exist');
+    });
   });
 });
