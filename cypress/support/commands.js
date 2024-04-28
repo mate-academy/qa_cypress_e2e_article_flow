f801af281b1fdadd86942aa51a88ab8272e7dbbb
Olha Myrutenko <ovka888@gmail.com>
2024-04-28 22:04:08 +0300

    Solution

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+/// <reference types='cypress' />
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -67,4 +68,8 @@ Cypress.Commands.add('createArticle', (title, description, body) => {
       }
     });
   });
+});
+
+Cypress.Commands.add('findByPlaceholder', (placeholder) => {
+  cy.get(`[placeholder="${placeholder}"]`);
 });
