95acfff8892ec70ac9b93c6abfebecbd2bd4408a
gajdziormax@gmail.com <gajdziormax@gmail.com>
2024-05-06 18:02:51 +0200

    Article flow testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("cypress");
-const faker = require('faker');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
@@ -13,6 +13,13 @@ module.exports = defineConfig({
             username: faker.name.firstName() + randomNumber,
             email: email.toLowerCase(),
             password: '12345Qwert!'
+          };
+        },
+        generateArticle() {
+          return{
+            title: 'Commodo sunt voluptate nisi cupidatat cillum labore consectetur quis. ',
+            description: 'Aliquip voluptate occaecat commodo labore ad veniam est. ',
+            body: 'Dolore dolore tempor adipisicing consequat magna ullamco nostrud commodo reprehenderit laboris. '
           };
         }
       });
--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,39 @@
-describe('', () => {
-  before(() => {
+describe('Page', () => {
 
+  let article;
+  let user;
+
+  beforeEach(() => {
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
+    })
+    cy.task('generateArticle').then((generatedArticle) => {
+      article = generatedArticle;
+    });
   });
 
-  it('', () => {
-
+  it('User should be able to create new article', () => {
+    cy.login(user.email, user.username, user.password)
+    cy.visit('/editor');
+    cy.findByPlaceholder('Article Title').type(article.title);
+    cy.findByPlaceholder(`What's this article about?`).type(article.body);
+    cy.findByPlaceholder('Write your article (in markdown)', 'textarea').type(article.description);
+    cy.findByPlaceholder('Enter tags').type(article.tag + '{enter}');
+    cy.get('.btn').click();
+    cy.get('h1').contains(article.title);
+    cy.get('.col-md-12').contains(article.description);
   });
 
-  it('', () => {
+  it.only('User should be able to delete article', () => {
+    const lowerCaseName = user.username.toLowerCase();
 
+    cy.login(user.email, user.username, user.password);
+    cy.createArticle(article.title, article.description, article.body);
+    cy.visit('/profile/' + lowerCaseName);
+    cy.visit('/profile/' + lowerCaseName);
+    cy.get('h1').contains(article.title).click();
+    cy.get('.btn').contains('Delete Article').click();
+    cy.visit('/profile/' + lowerCaseName);
+    cy.get('.article-preview').contains('No articles are here... yet.');
   });
 });
