d2abac5c40ada89bbfb283590944c76a2e029679
Maksymm <makshack10@gmail.com>
2024-05-14 23:57:02 +0300

    Task

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require("cypress");
-const faker = require('faker');
+const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
@@ -10,9 +10,17 @@ module.exports = defineConfig({
           const email = faker.internet.email();
           const randomNumber = Math.floor(Math.random(1000) * 1000);
           return {
-            username: faker.name.firstName() + randomNumber,
+            username: faker.name.firstName().toLowerCase() + randomNumber,
             email: email.toLowerCase(),
             password: '12345Qwert!'
+          };
+        },
+        generateArticl() {
+          return {
+            title: faker.lorem.word(10),
+            description: faker.lorem.words({ min: 5, max: 15 }),
+            body: faker.lorem.words(10),
+            tagList: faker.lorem.word()
           };
         }
       });
