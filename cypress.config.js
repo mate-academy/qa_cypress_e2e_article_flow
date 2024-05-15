be539fc6f95990a5f5d37bd931c633d3bf510e2a
davatrava <davatrava@gmail.com>
2024-05-15 14:45:22 +0300

    Working with Article

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require("cypress");
-const faker = require('faker');
+const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
@@ -13,6 +13,14 @@ module.exports = defineConfig({
             username: faker.name.firstName() + randomNumber,
             email: email.toLowerCase(),
             password: '12345Qwert!'
+          };
+        },
+        generateArticle() {
+          return {
+            title: faker.lorem.word(1),
+            description: faker.lorem.words(5),
+            body: faker.lorem.words(10),
+            tags: faker.lorem.word(4)
           };
         }
       });
--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,40 @@
-describe('', () => {
-  before(() => {
-
+/* eslint-disable max-len */
+describe('should provide the ability to work with article', () => {
+  let user;
+  let article;
+  beforeEach(() => {
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
+    cy.visit('/');
   });
 
-  it('', () => {
-
+  it('should provide an ability to create new article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('/editor');
+    cy.findByPlaceholder('Article Title').type(article.title);
+    cy.findByPlaceholder('What\'s this article about?')
+      .type(article.description);
+    cy.findByPlaceholder('Write your article (in markdown)')
+      .type(article.body);
+    cy.findByPlaceholder('Enter tags')
+      .type(article.tags);
+    cy.get('.btn.btn-lg').contains('Publish Article').should('exist').click();
+    cy.get('.btn.btn-lg').contains('Publish Article').should('exist').click();
+    cy.get('.banner').should('contain', article.title);
   });
 
-  it('', () => {
-
+  it('should provide an ability to delete article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.createArticle(article.title, article.description, article.body)
+      .then((response) => {
+        const slug = response.body.article.slug;
+        cy.visit(`/article/${slug}`);
+      });
+    cy.get('.btn').contains('Delete Article').should('exist').click();
+    cy.get('.article-preview').should('contain', 'No articles are here... yet.');
   });
 });
