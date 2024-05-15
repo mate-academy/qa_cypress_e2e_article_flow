022f29bde64bdd4e749c53cb9b4ee43b6e4c91ce
Yurii Mohylnytskyi <yuriistarlife@gmail.com>
2024-05-15 19:16:43 +0300

    I think it is Done

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,40 @@
-const { defineConfig } = require("cypress");
-const faker = require('faker');
-
+const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://conduit.mate.academy/',
     setupNodeEvents(on, config) {
       on('task', {
         generateUser() {
-          const email = faker.internet.email();
+          function generateRandomEnglishString(length) {
+            const characters =
+              'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+            let randomString = '';
+
+            for (let i = 0; i < length; i++) {
+              const randomIndex = Math.floor(Math.random() * characters.length);
+              randomString += characters[randomIndex];
+            }
+
+            return randomString;
+          }
+
+          function generateRandomEmail() {
+            const firstName = generateRandomEnglishString(5);
+            const lastName = generateRandomEnglishString(10);
+            const domain1 = generateRandomEnglishString(7);
+            const domain2 = generateRandomEnglishString(4);
+            return `${firstName}.${lastName}@${domain1}.${domain2}`;
+          }
+          const email = generateRandomEmail();
           const randomNumber = Math.floor(Math.random(1000) * 1000);
           return {
-            username: faker.name.firstName() + randomNumber,
+            username: generateRandomEnglishString(7) + randomNumber,
             email: email.toLowerCase(),
-            password: '12345Qwert!'
+            password: '12345Qwert!',
+            artTitle: generateRandomEnglishString(7) + randomNumber,
+            artDescribe: generateRandomEnglishString(15) + randomNumber,
+            artBody: generateRandomEnglishString(30) + randomNumber
           };
         }
       });
--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,45 @@
-describe('', () => {
-  before(() => {
+describe('Conduit user can', () => {
+  let user;
 
+  beforeEach(() => {
+    cy.visit('');
+    cy.task('generateUser').then((generateUser) => {
+      cy.log(generateUser);
+      user = generateUser;
+    });
+    cy.log(user);
   });
 
-  it('', () => {
+  const texts = {
+    phArtTitle: 'Article Title',
+    phArtDescribe: 'What\'s this article about?',
+    phArtBody: 'Write your article',
+    phArtTags: 'Enter tags',
+    btnDelete: 'Delete Article',
+    noArtMessage: 'No articles are here... yet.'
+  };
 
+  it('create new artile', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('');
+    cy.get('.ion-compose').click();
+    cy.getPlaceholder(texts.phArtTitle).type(user.artTitle);
+    cy.getPlaceholder(texts.phArtDescribe).type(user.artDescribe);
+    cy.getPlaceholder(texts.phArtBody).type(user.artBody);
+    cy.getPlaceholder(texts.phArtTags).type(`${user.artTitle}{enter}`);
+    cy.get('[type="button"]').click();
+    cy.get('.col-md-12').should('contain', user.artBody);
   });
 
-  it('', () => {
-
+  it('delete article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('');
+    cy.createArticle(user.artTitle, user.artDescribe, user.artBody);
+    cy.contains('.nav-link', user.username.toLowerCase()).click();
+    cy.contains('h1', user.artTitle).click();
+    cy.contains('.btn', texts.btnDelete).click();
+    cy.contains('.nav-link', user.username.toLowerCase()).click();
+    cy.reload();
+    cy.get('.article-preview').should('contain', texts.noArtMessage);
   });
 });
