2658f209c305ee2a791c9ed144fdcc5085b6bf7a
Ivan <panait.vanya@gmail.com$>
2024-05-15 14:06:18 +0300

    Article

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,22 @@
-describe('', () => {
+/// <reference types='cypress' />
+describe('Check article flow', () => {
+  let user;
+  const title = "Title";
+  const description = "Article Description";
+  const body = " Article Body";
   before(() => {
-
+    cy.visit('/user/login');
+    cy.task('generateUser').then((userData) => {
+      user = userData;
+    });
   });
-
-  it('', () => {
-
-  });
-
-  it('', () => {
-
-  });
+  it('should logged-in user', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('');
+    
+    it('should create an article', () => {
+    cy.createArticle(title, description, body);
+    
+});
+});
 });
