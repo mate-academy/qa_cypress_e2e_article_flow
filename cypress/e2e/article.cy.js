4c3a42214a465e60387f7596b361ffa08d77dd45
Ihor Sokolenko <igorsokolenko1986@gmail.com>
2024-05-16 16:57:17 +0300

    article_flow

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -9,21 +9,20 @@ describe('Conduit', () => {
     cy.log(user);
   });
 
-  it('Create article', () => {
+  it('should create an article', () => {
     cy.login(user.email, user.username, user.password);
     cy.visit('');
     cy.get('.ion-compose').click();
     cy.findByPlaceholder('Article Title').type('Test');
-    cy.findByPlaceholder("What's").type('Test1234');
+    cy.findByPlaceholder('What\'s').type('Test1234');
     cy.findByPlaceholder('Write').type('Test about test');
     cy.findByPlaceholder('Enter').type(`Test666{enter}`);
     cy.get('[type="button"]').click();
-    cy.visit('');
-    cy.get('.nav > :nth-child(2) > .link').click();
-    cy.get(':nth-child(2) > .preview-link > h1').should('be.visible');
+    cy.get('h1').should('contain', 'Test');
+    cy.get('.ion-edit').eq(0).should('exist');
   });
 
-  it('Delete test', () => {
+  it('should delete an article', () => {
     cy.login(user.email, user.username, user.password);
     cy.visit('');
     cy.createArticle(
