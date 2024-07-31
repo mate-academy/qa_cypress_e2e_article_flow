a96c9d43b8f41a01bb8340deedd60d5577e213ef
retur <return0fthekingv@gmail.com>
2024-07-31 17:57:52 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require("cypress");
-const faker = require('faker');
+const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
@@ -10,9 +10,20 @@ module.exports = defineConfig({
           const email = faker.internet.email();
           const randomNumber = Math.floor(Math.random(1000) * 1000);
           return {
-            username: faker.name.firstName() + randomNumber,
+            username: faker.person.firstName() + randomNumber,
             email: email.toLowerCase(),
             password: '12345Qwert!'
+          };
+        }
+      });
+      on('task', {
+        generateArticle() {
+          const randomNumber = Math.floor(Math.random() * 1000);
+          return {
+            title: faker.lorem.word() + randomNumber,
+            description: faker.lorem.words(),
+            body: faker.lorem.word(),
+            tag: faker.lorem.word()
           };
         }
       });
--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,43 @@
-describe('', () => {
-  before(() => {
+/* eslint-disable max-len */
+import { faker } from "@faker-js/faker";
 
+/// <reference types="cypress" />
+
+describe('Conduit', () => {
+  let user;
+  let article;
+
+  beforeEach(() => {
+    cy.task('generateUser').then((generateUser) => {
+      user = generateUser;
+    });
+    cy.task('generateArticle').then((generateArticle) => {
+      article = generateArticle;
+    });
   });
 
-  it('', () => {
+  it('should create the article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('');
+    cy.contains('.nav-link', 'New Article').click();
+    cy.findByPlaceholder('Article Title').type(article.title);
+    cy.findByPlaceholder('What\'s this article about?')
+      .type(article.description);
+    cy.findByPlaceholder('Write your article').type(article.body);
+    cy.findByPlaceholder('Enter tags').type(`${article.tag}{enter}`);
 
+    cy.get('[type="button"]').click();
+    cy.url().should('contain', article.title);
+    cy.get('h1').should('contain', article.title);
   });
 
-  it('', () => {
-
+  it('should allow to delete the article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.createArticle(article.title, article.description, article.body, article.tagList);
+    cy.visit('');
+    cy.get(':nth-child(4) > .nav-link').click();
+    cy.contains('h1', article.title).click();
+    cy.contains('button', 'Delete Article').click();
+    cy.url().should('not.contain', article.title);
   });
 });
