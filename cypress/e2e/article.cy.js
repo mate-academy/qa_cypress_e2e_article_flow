7d254d9ad2b7a3607c2309b5a72ba5e9841a65b9
MishaKluch <nimamiha@gmail.com>
2024-07-31 14:35:24 +0300

    Article tests

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,49 @@
-describe('', () => {
-  before(() => {
+/// <reference types="cypress" />
+import { ar } from "@faker-js/faker";
+import { generateArticle } from "../support/generateArticle";
 
+describe("Conduit", () => {
+  const article = generateArticle();
+  let articleUrl;
+  const user = {
+    name: "qamay2024",
+    email: "qamay2024@gmail.com",
+    password: "1234567890",
+  };
+  beforeEach(() => {
+    cy.login(user.email, user.password);
+    cy.visit("/");
   });
 
-  it('', () => {
+  it("should login user with valid credentials", () => {
+    // cy.visit('user/login')
+    // cy.findByPlaceholder('Email').type(user.email);
+    // cy.findByPlaceholder('Password').type(user.password);
+    // cy.contains('[type="submit"]', 'Sign in').click();
+    // cy.get('.btn').contains('Sign in').click();
+    cy.get(".nav-link").contains(user.name).should("be.visible");
+  });
+  it("should allow user article creation", () => {
+    cy.contains(".nav-link", "New Article").click();
+    cy.findByPlaceholder("Article Title").type(article.title);
+    cy.findByPlaceholder("What's this article about?").type(
+      article.description
+    );
+    cy.findByPlaceholder("Write your article (in markdown)").type(article.body);
+    cy.findByPlaceholder("Enter tags").type(article.tag);
+    cy.findByPlaceholder("Enter tags").type("{enter}");
+    cy.get(".btn").click();
 
+    cy.url().should("contain", article.title);
+    cy.get("h1").should("contain", article.title);
+    cy.url().then((createdArticleUrl) => {
+      articleUrl = createdArticleUrl;
+    });
   });
 
-  it('', () => {
-
+  it("should allow deleting created user article", () => {
+    cy.visit(articleUrl);
+    cy.contains(".btn", "Delete Article").click();
+    cy.url().should("not.include", "/article");
   });
 });
---
+++ cypress/support/generateArticle.js
@@ -0,0 +1,12 @@
+const { faker } = require('@faker-js/faker');
+
+const generateArticle = () => {
+    const randomNumber = Math.floor(Math.random() * 1000);
+    return { 
+        title: faker.lorem.word() + randomNumber,
+        description: faker.lorem.word(), 
+        body: faker.lorem.word(), 
+        tag: faker.lorem.word()
+    };
+};
+module.exports = { generateArticle };
