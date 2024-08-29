3293c20c464444685bcbba3a38d2bc26f84f6965
Bl00mer <bl00mer@MacBook-Pro-Bl00mer.local>
2024-08-29 15:56:41 +0200

    Solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,26 @@
-describe('', () => {
+describe("Login and article delete functions", () => {
   before(() => {
-
+    cy.visit("https://conduit.mate.academy/user/login");
+    cy.get('input[placeholder="Email"]').type("bartek@gmail.com");
+    cy.get('input[placeholder="Password"]').type("bartek");
+    cy.get('button[type="submit"]').click();
   });
 
-  it('', () => {
+  it("Should create an article and then delete the article", () => {
+    cy.get("a").contains("New Article").click();
+    cy.get('input[placeholder="Article Title"]').type("test title");
+    cy.get('input[placeholder="What\'s this article about?"]').type(
+      "about a test"
+    );
+    cy.get('textarea[placeholder="Write your article (in markdown)"]').type(
+      "it is an article"
+    );
+    cy.get('button[type="button"]').contains("Publish Article").click();
 
-  });
-
-  it('', () => {
-
+    cy.contains("test title")
+      .parent()
+      .find("button.btn-outline-danger")
+      .contains("Delete Article")
+      .click();
   });
 });
