73f4c0fd020595b6e624b502d9fdf5c3c16ec61e
Bozhenko Roman <bozhenko091999@gmail.com>
2024-09-09 16:43:19 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,23 @@
 const { defineConfig } = require('cypress');
 // const faker = require('faker');
+const user = {
+  username: '',
+  email: '',
+  password: ''
+};
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://conduit.mate.academy/',
     setupNodeEvents(on, config) {
       on('task', {
-        generateUser() {
+        generateUser: () => {
           const randomNumber = Math.floor(Math.random(1000) * 1000);
           const email = `test${randomNumber}@domain.com`;// faker.internet.email();
-          return {
-            username: 'TestUsername' + randomNumber, // faker.name.firstName() + randomNumber,
-            email: email.toLowerCase(),
-            password: '12345Qwert!'
-          };
+          user.username = 'TestUsername' + randomNumber;
+          user.email = email.toLowerCase();
+          user.password = '12345Qwert!';
+          return user;
         }
       });
     }
--- cypress/e2e/article.spec.cy.js
+++ cypress/e2e/article.spec.cy.js
@@ -1,77 +1,84 @@
 /// <reference types='cypress' />
 
+let email;
+let password;
+
+function loginUser() {
+  cy.visit('/');
+  cy.get(`a[href='/user/login']`)
+    .click();
+  cy.get(`input[type='email']`)
+    .type(email);
+  cy.get(`input[type='password']`)
+    .type(password);
+  cy.get(`button[type='submit']`)
+    .click();
+}
+
 describe('User', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
-  let username;
-  let email;
-  let password;
+  const aTitle = 'Test Title';
+  const aAbout = 'Test About';
+  const aText = 'Test Article';
+  const aTag = 'Test';
+
+  let profileLink;
 
   it(`should be able to register`, () => {
     cy.task('generateUser').then((user) => {
-      username = user.username;
-      email = user.email;
-      password = user.password;
-
       cy.get(`a[href='/user/register']`)
         .click();
       cy.get(`input[type='text']`)
-        .type(username);
+        .type(user.username);
       cy.get(`input[type='email']`)
-        .type(email);
+        .type(user.email);
       cy.get(`input[type='password']`)
-        .type(password);
+        .type(user.password);
       cy.get(`button[type='submit']`)
         .click();
+
+      profileLink = '/profile/' + user.username.toLowerCase();
+      email = user.email;
+      password = user.password;
+
+      cy.get(`a[href='${profileLink}']`)
+        .should('exist');
     });
   });
 
   it('should be able to create article', () => {
-    cy.get(`a[href='/user/login']`)
-      .click();
-    cy.get(`input[type='email']`)
-      .type(email);
-    cy.get(`input[type='password']`)
-      .type(password);
-    cy.get(`button[type='submit']`)
-      .click();
+    loginUser();
 
     cy.get(`a[href='/editor']`)
       .click();
     cy.get(`input[placeholder='Article Title']`)
-      .type('Test Title');
+      .type(aTitle);
     cy.get(`input[placeholder="What's this article about?"]`)
-      .type('Test About');
+      .type(aAbout);
     cy.get(`textarea[placeholder='Write your article (in markdown)']`)
-      .type('Test Article');
+      .type(aText);
     cy.get(`input[placeholder='Enter tags']`)
-      .type('Test');
+      .type(aTag);
     cy.get(`button[type='button']`)
       .click();
     cy.get(`button[type='button']`)
       .click();
 
-    cy.get(`a[href='/profile/${username.toLowerCase()}']`)
+    cy.get(`a[href='${profileLink}']`)
       .click();
-    cy.get(`h1`).contains('Test Title')
+    cy.get(`h1`).contains(aTitle)
       .should('exist');
   });
 
   it('should be able to delete article', () => {
-    cy.get(`a[href='/user/login']`)
-      .click();
-    cy.get(`input[type='email']`)
-      .type(email);
-    cy.get(`input[type='password']`)
-      .type(password);
-    cy.get(`button[type='submit']`)
-      .click();
+    loginUser();
 
-    cy.get(`a[href='/profile/${username.toLowerCase()}']`)
+    cy.get(`a[href='${profileLink}']`)
       .click();
-    cy.get(`h1`).contains('Test Title')
+    cy.get(`h1`).contains(aTitle)
       .should('exist')
       .click();
     cy.get(`button`)
@@ -80,7 +87,7 @@ describe('User', () => {
 
     cy.visit('/');
 
-    cy.get(`a[href='/profile/${username.toLowerCase()}']`)
+    cy.get(`a[href='${profileLink}']`)
       .eq(0)
       .click();
     cy.get(`div`)
