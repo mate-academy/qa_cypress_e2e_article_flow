4465f523c4e4de919ef6fda5c61b2a7006ebfeed
nevermore <gritsanowo@gmail.com>
2024-10-14 23:59:28 +0300

    solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,21 @@
-describe('', () => {
-  before(() => {
+/// <reference types='cypress' />
 
+describe('Conduit', () => {
+  const userName = 'nevermore';
+  const email = 'nevermore@gmail.com';
+  const password = 'nevermore123';
+  beforeEach(() => {
+    cy.visit('/');
+    cy.login(email, userName, password);
   });
 
-  it('', () => {
-
+  it('should create article with valid data', () => {
+    cy.createArticle('title', 'description', 'body');
   });
 
-  it('', () => {
-
+  it('should delete article', () => {
+    cy.contains('a', 'Global Feed').click();
+    cy.contains('title').click();
+    cy.get('.article-actions').find('.btn-outline-danger').click();
   });
 });
