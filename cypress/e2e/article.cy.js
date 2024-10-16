e9984c9320920685cd52c306d4e166f29a2f62c8
Vlad <lukashukvlad534@gmail.com>
2024-10-16 16:44:22 +0300

    article flow autotests changed2

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -2,12 +2,14 @@
 
 let user;
 let article;
+
 describe('Conduit', () => {
   beforeEach(() => {
     cy.visit('');
     cy.task('generateUser').then((generateUser) => {
       user = generateUser;
     });
+
     cy.task('generateArticle').then((generateArticle) => {
       article = generateArticle;
     });
@@ -15,11 +17,13 @@ describe('Conduit', () => {
 
   it('should login and create article', () => {
     cy.login(user.email, user.username, user.password);
+
     cy.visit('/editor');
     cy.get('[placeholder="Article Title"]').type(article.title);
     cy.get('[placeholder="What\'s this article about?"]').type(article.desc);
     cy.get('[placeholder="Write your article (in markdown)"]')
       .type(article.body);
+
     cy.contains('.btn', 'Publish Article').click();
 
     cy.contains('.container', article.title).should('exist');
@@ -27,11 +31,14 @@ describe('Conduit', () => {
 
   it('should delete the article', () => {
     cy.login(user.email, user.username, user.password);
+
     cy.createArticle(article.title, article.desc, article.body)
       .then((response) => {
         const slug = response.body.article.slug;
+
         cy.visit(`article/${slug}`);
       });
+
     cy.contains('.btn', 'Delete Article').click();
 
     cy.contains('.nav-link', 'Global Feed').should('be.visible');
