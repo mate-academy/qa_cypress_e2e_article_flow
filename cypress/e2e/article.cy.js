9c9205860483f6ca133e3578ca7ebf2e35c433dc
luckezQA <labchuk2007@gmail.com>
2024-10-16 17:03:06 +0300

    Solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,38 +1,33 @@
-/// <reference types='cypress' />
+const { faker } = require('@faker-js/faker');
 
-let user;
-let article;
-
-describe('Conduit', () => {
-  beforeEach(() => {
-    cy.visit('');
-    cy.task('generateUser').then((generateUser) => {
-      user = generateUser;
-    });
-
-    cy.task('generateArticle').then((generateArticle) => {
-      article = generateArticle;
-    });
+describe('Article CRUD', () => {
+  before(() => {
   });
+  const articleTitle = faker.word.words(1);
+  const articleTopic = faker.word.words(2);
+  const articleDescription = faker.word.words(5);
+  const articleTag = faker.word.words(1);
 
-  it('should login and create article', () => {
-    cy.login(user.email, user.username, user.password);
-
-    cy.visit('/editor');
-    cy.get('[placeholder="Article Title"]').type(article.title);
-    cy.get('[placeholder="What\'s this article about?"]').type(article.desc);
+  it('should create new article', () => {
+    cy.task('generateUser').then((user) => {
+      cy.login(user.email, user.username, user.password);
+    });
+    cy.contains('.nav-link', 'New Article').click();
+    cy.get('[placeholder="Article Title"]').type(articleTitle);
+    cy.get('[placeholder="What\'s this article about?"]').type(articleTopic);
     cy.get('[placeholder="Write your article (in markdown)"]')
-      .type(article.body);
-
+      .type(articleDescription);
+    cy.get('[placeholder="Enter tags"]').type(`${articleTag}{enter}`);
     cy.contains('.btn', 'Publish Article').click();
-
-    cy.contains('.container', article.title).should('exist');
+    cy.url().should('contain', 'article/');
   });
 
   it('should delete the article', () => {
-    cy.login(user.email, user.username, user.password);
+    cy.task('generateUser').then((user) => {
+      cy.login(user.email, user.username, user.password);
+    });
 
-    cy.createArticle(article.title, article.desc, article.body)
+    cy.createArticle(articleTitle, articleTopic, articleDescription)
       .then((response) => {
         const slug = response.body.article.slug;
 
