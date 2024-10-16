5bcc2c28fde2cfacd551dc83c5940e4f7cae0031
Iryna Turaieva <irina.onlyjob@gmail.com>
2024-10-16 14:14:36 +0200

    Cypress: Article flow_updated

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -22,18 +22,29 @@ describe('Article flow', () => {
   it('should create the article', () => {
     cy.get('a.nav-link[href="/editor"]').click();
     cy.get('[placeholder="Article Title"]')
-      .should('exist')
+      .should('exist');
+    cy.get('[placeholder="Article Title"]')
       .type(articleTitle);
-    cy.get('a.nav-link[href="/editor"]').click();
+    cy.get('[placeholder="Article Title"]')
+      .should('have.value', articleTitle);
     cy.get('[placeholder="What\'s this article about?"]')
-      .should('exist')
+      .should('exist');
+    cy.get('[placeholder="What\'s this article about?"]')
       .type(articleDescription);
+    cy.get('[placeholder="What\'s this article about?"]')
+      .should('have.value', articleDescription);
     cy.get('[placeholder="Write your article (in markdown)"]')
-      .should('exist')
+      .should('exist');
+    cy.get('[placeholder="Write your article (in markdown)"]')
       .type(articleBody);
+    cy.get('[placeholder="Write your article (in markdown)"]')
+      .should('have.value', articleBody);
     cy.get('[placeholder="Enter tags"]')
-      .should('exist')
+      .should('exist');
+    cy.get('[placeholder="Enter tags"]')
       .type(articleTags);
+    cy.get('[placeholder="Enter tags"]')
+      .should('have.value', articleTags);
     cy.get('.btn').contains('Publish')
       .should('exist')
       .and('not.be.disabled')
@@ -47,6 +58,8 @@ describe('Article flow', () => {
         expect(response.status).to.eq(200);
         articleSlug = response.body.article.slug;
         cy.visit(`/article/${articleSlug}`);
+        cy.get('h1').should('have.text', articleTitle);
+        cy.get('.article-content').contains(articleBody).should('exist');
         cy.intercept('DELETE', `/api/articles/${articleSlug}`).as('deleteArticle');
         cy.get('.btn').contains('Delete Article').click();
         cy.wait('@deleteArticle').its('response.statusCode').should('eq', 204);
