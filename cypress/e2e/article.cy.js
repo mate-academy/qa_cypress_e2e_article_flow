6ca4ad688e4e7f9e5b4e830178a69f6db863c961
IrynaT <zlata120707@gmail.com>
2024-10-29 09:30:54 +0200

    solution1

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -12,50 +12,43 @@ describe('Conduit article', () => {
     });
   });
 
-  it('', () => {
-    it('should allow for the creation of an article.', () => {
-      cy.login(user.email, user.username, user.password);
+  it('should allow for the creation of an article.', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.visit('/editor');
 
-      cy.visit('/editor');
+    cy.get('[placeholder="Article Title"]')
+      .type(articleData.title);
 
-      cy.get('[placeholder="Article Title"]')
-        .type(articleData.title);
+    cy.get('[placeholder="What\'s this article about?"]')
+      .type(articleData.description);
 
-      cy.get('[placeholder="What\'s this article about?"]')
-        .type(articleData.description);
+    cy.get('[placeholder="Write your article (in markdown)"]')
+      .type(articleData.body);
 
-      cy.get('[placeholder="Write your article (in markdown)"]')
-        .type(articleData.body);
+    cy.contains('.btn', 'Publish Article')
+      .click();
 
-      cy.contains('.btn', 'Publish Article')
-        .click();
+    cy.contains('.container', articleData.title)
+      .should('exist');
+  });
 
-      cy.contains('.container', articleData.title)
-        .should('exist');
-    });
-
-    it('', () => {
-      it('should allow for the deletion of the article', () => {
-        cy.login(user.email, user.username, user.password);
-
-        cy.createArticle(articleData.title,
-          articleData.description, articleData.body)
-          .then((response) => {
-            const slug = response.body.article.slug;
-
-            cy.visit(`article/${slug}`);
-          });
-
-        cy.contains('.btn', 'Delete Article')
-          .eq(0)
-          .click();
-
-        cy.contains('.nav-link', 'Global Feed')
-          .should('be.visible');
-
-        cy.get('.article-preview')
-          .should('contain.text', 'No articles are here... yet');
+  it('should allow for the deletion of the article', () => {
+    cy.login(user.email, user.username, user.password);
+    cy.createArticle(articleData.title, articleData
+      .description, articleData.body)
+      .then((response) => {
+        const slug = response.body.article.slug;
+        cy.visit(`article/${slug}`);
       });
-    });
+
+    cy.contains('.btn', 'Delete Article')
+      .eq(0)
+      .click();
+
+    cy.contains('.nav-link', 'Global Feed')
+      .should('be.visible');
+
+    cy.get('.article-preview')
+      .should('contain.text', 'No articles are here... yet');
   });
 });
