b0464fd68e641cd42df384848c3f05e3f6184264
Misha <banderwolfe06@gmail.com>
2024-11-09 11:24:08 +0100

    update

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,31 @@
-describe('', () => {
-  before(() => {
+const { faker } = require('@faker-js/faker');
 
-  });
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://conduit.mate.academy/');
 
-  it('', () => {
+    const username = 'hupaqorage@mailinator.com';
+    const password = 'Pa$$w0rd!';
 
-  });
+    cy.get(':nth-child(2) > .nav-link').click();
+    cy.get(':nth-child(1) > .form-control').type(username);
+    cy.get(':nth-child(2) > .form-control').type(password);
+    cy.get('.btn').click();
 
-  it('', () => {
+    cy.get(':nth-child(1) > .nav-link').click();
 
+    cy.visit('https://conduit.mate.academy/editor');
+
+    cy.get(':nth-child(1) > .form-control').type(faker.lorem.words(3));
+    cy.get(':nth-child(2) > .form-control').type(faker.lorem.words(5));
+    cy.get(':nth-child(3) > .form-control').type(faker.lorem.words(15));
+    cy.get(':nth-child(4) > .form-control').type(faker.lorem.words(1));
+
+    cy.get('.btn').click();
+    cy.get('.btn').click();
+
+    cy.get(
+      '.article-actions > .article-meta > :nth-child(3) > .btn-outline-danger'
+    ).click();
   });
 });
