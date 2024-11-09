e96568c4f61af6d71980cf97d09513b15f7351ac
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-11-09 18:07:30 +0200

    try

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 const faker = require('faker');
 
 module.exports = defineConfig({
@@ -8,11 +8,18 @@ module.exports = defineConfig({
       on('task', {
         generateUser() {
           const email = faker.internet.email();
-          const randomNumber = Math.floor(Math.random(1000) * 1000);
+          const randomNumber = Math.floor(Math.random() * 1000);
           return {
             username: faker.name.firstName() + randomNumber,
             email: email.toLowerCase(),
             password: '12345Qwert!'
+          };
+        },
+        articleInput() { 
+          return {
+            title: faker.lorem.sentence(),
+            description: faker.lorem.paragraph(),
+            body: faker.lorem.paragraphs(3)
           };
         }
       });
