da2219d46afaf0226af61f7b1870ddc2fa9f92aa
Galers <galosaandrew@gmail.com>
2025-01-11 15:58:40 +0200

    test: Add tests

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,51 @@
-describe('', () => {
-  before(() => {
+/// <reference types="cypress" />
 
+describe('Article Management', () => {
+  let user;
+  let article;
+
+  beforeEach(() => {
+    cy.visit('/');
+    cy.location('protocol').should('eq', 'https:');
+    cy.title().should('eq', 'Conduit');
+
+    cy.task('generateUser').then((generateUser) => {
+      cy.expectData(generateUser, 'email');
+      cy.expectData(generateUser, 'username');
+      cy.expectData(generateUser, 'password');
+      user = generateUser;
+      cy.login(user.email, user.username, user.password);
+    });
+
+    cy.task('generateArticle').then((generateArticle) => {
+      cy.createArticle(
+        generateArticle.title,
+        generateArticle.description,
+        generateArticle.body
+      );
+      article = generateArticle;
+    });
   });
 
-  it('', () => {
-
+  it('should display the newly created article on the profile', () => {
+    cy.visit(`/profile/${user.username.toLowerCase()}`);
+    cy.get('.article-preview')
+      .should('contain.text', article.title)
+      .and('contain.text', article.description);
   });
 
-  it('', () => {
-
+  it('should add a new article', () => {
+    cy.visit(`/profile/${user.username.toLowerCase()}`);
+    cy.get('h1').should('contain.text', article.title).click();
+    cy.get('[class="btn btn-outline-danger btn-sm"]').first().click();
+    cy.on('window:confirm', (message) => {
+      expect(message).to.equal('Do you really want to delete it?');
+      return true;
+    });
+    cy.visit(`/profile/${user.username.toLowerCase()}`);
+    cy.get('.article-preview').should(
+      'contain.text',
+      'No articles are here... yet.'
+    );
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,29 +1,4 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
+/// <reference types="cypress" />
 const imgUrl = 'https://static.productionready.io/images/smiley-cyrus.jpg';
 
 Cypress.Commands.add('login', (email, username, password) => {
@@ -67,4 +42,12 @@ Cypress.Commands.add('createArticle', (title, description, body) => {
       }
     });
   });
+});
+
+Cypress.Commands.add('typeInput', (type, value) => {
+  cy.get(`input[type=${type}]`).type(value);
+});
+
+Cypress.Commands.add('expectData', (user, value) => {
+  expect(user).to.have.property(value);
 });
