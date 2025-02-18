c3cfd82b64bdd37bdaeb6adb2a523ab65a04883b
Nazar Bodak <nazarb@bookmap.com>
2025-02-18 17:58:21 +0200

    changes

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -2,28 +2,35 @@
 import { faker } from '@faker-js/faker';
 
 const { generateArticleData } = require('../support/generateArticleData');
-const randomUser = {
-  username: faker.person.firstName() + faker.number.int(),
-  email: faker.internet.email(),
-  password: faker.internet.password()
-};
-
+function generateRandomUser() {
+  return {
+    username: faker.person.firstName() + faker.number.int(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  };
+}
 describe('Article testing', () => {
   before(() => {
-    cy.visit('/');
+    const randomUser = generateRandomUser();
     cy.login(randomUser.email, randomUser.username, randomUser.password);
+    cy.visit('/');
   });
 
   it('should create an article with valid data', () => {
+    const randomUser = generateRandomUser();
+    cy.login(randomUser.email, randomUser.username, randomUser.password);
     cy.visit('/editor');
     const articleData = generateArticleData();
     cy.createArticleUI(articleData);
     cy.verifyArticleData(articleData);
   });
 
-  it('should delete an article', () => {
+  it('should delete an article from Global Feed', () => {
+    const randomUser = generateRandomUser();
+    cy.login(randomUser.email, randomUser.username, randomUser.password);
     const articleData = generateArticleData();
     cy.createArticleAPI(articleData);
+    cy.visit('/');
     cy.reload();
     cy.goGlobalFeed();
     cy.contains(articleData.title).click();
