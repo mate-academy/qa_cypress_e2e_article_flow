b189e93279544481b30b6b6e4050e723debea8fd
Nazar Bodak <nazarb@bookmap.com>
2025-02-18 17:35:49 +0200

    solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,35 @@
-describe('', () => {
+/// <reference types='cypress' />
+import { faker } from '@faker-js/faker';
+
+const { generateArticleData } = require('../support/generateArticleData');
+const randomUser = {
+  username: faker.person.firstName() + faker.number.int(),
+  email: faker.internet.email(),
+  password: faker.internet.password()
+};
+
+describe('Article testing', () => {
   before(() => {
-
+    cy.visit('/');
+    cy.login(randomUser.email, randomUser.username, randomUser.password);
   });
 
-  it('', () => {
-
+  it('should create an article with valid data', () => {
+    cy.visit('/editor');
+    const articleData = generateArticleData();
+    cy.createArticleUI(articleData);
+    cy.verifyArticleData(articleData);
   });
 
-  it('', () => {
-
+  it('should delete an article', () => {
+    const articleData = generateArticleData();
+    cy.createArticleAPI(articleData);
+    cy.reload();
+    cy.goGlobalFeed();
+    cy.contains(articleData.title).click();
+    cy.contains('button', 'Delete Article').click();
+    cy.goGlobalFeed();
+    cy.reload();
+    cy.contains(articleData.title).should('not.exist');
   });
 });
