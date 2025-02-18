83b1f0b5632f3d154c49a6243cf55687aaf63702
Dmytro Ivasenko <dmtivasenko@gmail.com>
2025-02-18 05:24:44 +0400

    add solution

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,37 @@
-describe('', () => {
+import { faker } from '@faker-js/faker';
+
+describe('User profile', () => {
+  const title = faker.lorem.sentence();
+  const description = faker.lorem.sentence();
+  const body = faker.lorem.text();
+  let user;
+
   before(() => {
-
+    cy.task('generateUser').then((response) => {
+      user = response;
+      cy.login(user.email, user.username, user.password);
+      cy.visit('/');
+    });
   });
 
-  it('', () => {
+  it('should create and delete an article', () => {
+    cy.createArticle(title, description, body).then(() => {
+      cy.get(`[href="/profile/${user.username.toLowerCase()}"]`)
+        .click();
+      cy.contains('.article-preview', description).should('be.visible');
+      cy.contains('.article-preview', title).click();
+      cy.get('.article-page').find('h1').contains(title).should('be.visible');
+      cy.get('.article-page').within(() => {
+        cy.contains('p', body).should('be.visible');
+      });
 
-  });
-
-  it('', () => {
-
+      cy.intercept('DELETE', '/api/articles/*').as('deleteArticle');
+      cy.contains('button.btn', 'Delete Article').click();
+      cy.wait('@deleteArticle');
+      cy.contains('.link', 'Global Feed').click();
+      cy.contains('.article-preview', title).should('not.exist');
+      cy.contains('.article-preview', description).should('not.exist');
+      cy.contains('.article-preview', body).should('not.exist');
+    });
   });
 });
